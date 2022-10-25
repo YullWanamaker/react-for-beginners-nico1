1e268cbb182a80c4464c03f9ae3c59c27f132c02
Wanamaker <dla4038@naver.com>
2022-10-25 17:39:50 +0900

    lastcoin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  const [loading, setLoading] = useState(ture);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.jason())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
       <h1>The Coins!</h1>
       {loading ? <strong>Loading...</strong> : null}
+      <ul>
+        {coins.map((coin) => (
+          <li>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}USD
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
