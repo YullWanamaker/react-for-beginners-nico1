bf7eab89641968d482a3e60ee7e415e703c3542a
Wanamaker <dla4038@naver.com>
2022-10-25 15:41:58 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import { useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
 
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search"
-      ></input>
-      <h1> {counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      <h1>My To Dos ({toDos.length}) </h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do"
+        />
+
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
-
 export default App;
